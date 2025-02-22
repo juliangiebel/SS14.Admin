58d15195c0b4825abbad09e7e99e907e5130c0b5
juliangiebel <juliangiebel@live.de>
2025-02-22 14:43:03 +0100

    Use cascading auth state Implement helper for getting role names from admin flags

---
+++ SS14.Admin/Helpers/AdminFlagRoleExtension.cs
@@ -0,0 +1,11 @@
+﻿using SS14.Admin.Admins;
+
+namespace SS14.Admin.Helpers;
+
+public static class AdminFlagRoleExtension
+{
+    public static string RoleName(this AdminFlags flag)
+    {
+        return flag.ToString().ToUpper();
+    }
+}
