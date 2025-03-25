a4a1afd799812432127d07aedf31da6564402b34
juliangiebel <juliangiebel@live.de>
2025-03-25 21:21:39 +0100

    Work on date filter

---
+++ SS14.Admin/Components/Forms/DatePicker.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace SS14.Admin.Components.Forms;
+
+public partial class DatePicker : ComponentBase, IAsyncDisposable
+{
+    private readonly IJSRuntime _js;
+    private IJSObjectReference? _instance;
+
+    [Parameter] public string? Label { get; set; }
+    [Parameter] public string? Value { get; set; }
+    [Parameter] public EventCallback<string?> ValueChanged { get; set; }
+
+
+    private readonly string _id = $"date-picker-{Guid.NewGuid().ToString()}";
+
+    public DatePicker(IJSRuntime js)
+    {
+        _js = js;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender)
+            return;
+
+        var module = await _js.InvokeAsync<IJSObjectReference>("import", "./Components/Forms/DatePicker.razor.js");
+
+        _instance = await module.InvokeAsync<IJSObjectReference>("DatePicker.init", DotNetObjectReference.Create(this), _id);
+        await module.DisposeAsync();
+    }
+
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_instance is null)
+            return;
+
+        try
+        {
+            await _instance.DisposeAsync();
+        }
+        catch (JSDisconnectedException)
+        {
+        }
+    }
+}
+
---
+++ SS14.Admin/Components/Forms/DatePicker.razor.js
@@ -0,0 +1,16 @@
+﻿
+
+export class DatePicker {
+    static init(ref, id)
+    {
+        return new DatePicker(ref, id);
+    }
+
+    constructor(ref, id)
+    {
+        this.ref = ref;
+        this.id = id;
+    }
+}
+
+window.DatePicker = DatePicker;
